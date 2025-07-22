8d47602c220628aaf86270d86cf712db51acc117
huzaifaparwaiz <huzaifaparwaiz82@gmail.com>
2025-07-23 00:25:42 +0500

    Add files via upload
    Add C++ program demonstrating if-else statements

---
+++ tut3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Find the min of two number
+int main(){
+    int i = 10;
+    int j = 5;
+
+if(i>j){
+    cout<< j;
+}
+else{
+    cout<< i;
+}
+return 0;
+}
